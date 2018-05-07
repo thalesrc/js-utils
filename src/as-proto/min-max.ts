b26de53d9e50a986dbb0f876deca45ead67e6322
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2018-05-07 19:10:27 +0300

    min-max added to Math as static

---
+++ src/as-proto/min-max.ts
@@ -0,0 +1,23 @@
+import { minMax } from "../min-max";
+
+declare global {
+  export interface Math {
+    /**
+     * #### Limits the value by given parameters
+     *
+     * * * *
+     * ```typescript
+     * import "@gen-tech/js-utils/dist/as-proto/min-max";
+     *
+     * const limitedValue = Math.minMax(200, 300, Math.random() * 1000);
+     * ```
+     * * * *
+     * @param min minimum limit
+     * @param max maximum limit
+     * @param value value to limit
+     */
+    minMax: typeof minMax;
+  }
+}
+
+Math.minMax = minMax;
--- src/min-max.ts
+++ src/min-max.ts
@@ -1,9 +1,17 @@
 /**
- * Limits the value
- * @param  {number} min minimum limit
- * @param  {number} max maximum limit
- * @param  {number} value value to limit
- * @returns number
+ * #### Limits the value by given parameters
+ *
+ * * * *
+ * Example usage:
+ * ```typescript
+ * import { minMax } from "@gen-tech/js-utils";
+ *
+ * const limitedValue = minMax(200, 300, Math.random() * 1000);
+ * ```
+ * * * *
+ * @param min minimum limit
+ * @param max maximum limit
+ * @param value value to limit
  */
 export function minMax(min: number, max: number, value: number): number {
   return Math.max(Math.min(max, value), min);
