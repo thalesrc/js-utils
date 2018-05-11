c9e428cca8316fd95b54917a066cc3feb931d55b
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2018-05-11 23:02:18 +0300

    tag update fixes

--- scripts/tag-update.js
+++ scripts/tag-update.js
@@ -1,16 +1,15 @@
 const updateJson = require('update-json');
-const latestTag = require('latest-git-tag');
 
-const [, , argVersion] = process.argv;
+const [, , version] = process.argv;
 
-Promise.resolve(argVersion)
-  .then(version => !version ? latestTag() : version)
-  .then(version => {
-    updateJson("./package.json", {version}, error => {
-      if (error) {
-        throw error;
-      } else {
-        console.log("Package version updated to:", version);
-      }
-    });
+if (version) {
+  updateJson("./package.json", {version}, error => {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Package version updated to:", version);
+    }
   });
+} else {
+  console.log("No tags found for this build, skipping version change");
+}
