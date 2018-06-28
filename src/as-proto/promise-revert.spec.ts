b890d79ea9dd55a373aa60c67941317dfcb6b7b6
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2018-06-28 15:55:38 +0300

    promise revert completed

---
+++ src/as-proto/promise-revert.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import 'mocha';
+
+import "./promise-revert";
+
+class CustomError {
+  constructor(public message: string) {}
+}
+
+describe('Promise Revert Proto Function', () => {
+  it('should throw error when its resolved', done => {
+    Promise.resolve("foo")
+      .revert()
+      .then(() => {
+        throw new CustomError("bar");
+      })
+      .catch(err => {
+        expect(err).to.eq("foo");
+        done();
+      });
+  });
+
+  it('should resolve when its rejected', done => {
+    Promise.reject("foo")
+      .revert()
+      .then(res => {
+        expect(res).to.eq("foo");
+        done();
+      });
+  });
+});
