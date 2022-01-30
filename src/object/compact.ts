bf0e03ba692234388c02fa370996c3261589a91c
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2022-01-30 15:09:09 +0300

    feat(Object): added additional values option to object.compact

--- src/object/compact.ts
+++ src/object/compact.ts
@@ -1,6 +1,8 @@
 /**
  * #### Removes `null` and `undefined` values and their keys from an object
  *
+ * Additional values can be removed by passing to an array as the second argument
+ *
  *  * * * *
  * Example usage:
  * ```typescript
@@ -29,13 +31,18 @@
  * ```
  * * * *
  * @param object Object delete empty keys
+ * @param additionalValuesToRemove Other values to delete
  * @returns Compacted object
  */
-export function compact<T extends Object>(object: T): Partial<T> {
+export function compact<T extends Record<string|number|symbol, any>>(
+  object: T,
+  additionalValuesToRemove: any[] = []
+): Partial<T> {
   const newObject = {...object};
+  const valuesToRemove = [undefined, null, ...additionalValuesToRemove];
 
   for (const [key, value] of Object.entries(newObject)) {
-    if (value === undefined || value === null) {
+    if (valuesToRemove.includes(value)) {
       delete newObject[key];
     }
   }
